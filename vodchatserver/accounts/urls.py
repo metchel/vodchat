7fd014b23cd5d7ef502b17b5b69f036f5b1405b7
metchel <matthew.etchells@mail.mcgill.ca>
2020-04-15 14:08:59 -0400

    Add back account stuff

--- vodchatserver/accounts/urls.py
+++ vodchatserver/accounts/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('',views.signup, name='signup'),
     path('login', views.do_login, name='login'),
     path('logout', views.do_logout, name='logout'),
+     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate_account, name='activate'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password/reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password/reset_sent.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password/resetting.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password/reset_successful.html'), name='password_reset_complete'),
 ]
--- vodchatserver/accounts/views.py
+++ vodchatserver/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.models import User
@@ -6,6 +6,12 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from . import forms
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.core.mail import EmailMessage
 
 def signup(request):
     context = {}
@@ -15,14 +21,40 @@ def signup(request):
             try:
               user = User.objects.create_user(
                 form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
                 password=form.cleaned_data['password'])
-              return HttpResponseRedirect(reverse('login'))
+              user.is_active = False
+              user.save()
+              current_site = get_current_site(request)
+              email_subject = 'Activate Your Account'
+              message = render_to_string('account_activation/activation.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),})
+              to_email = form.cleaned_data.get('email')
+              email = EmailMessage(email_subject, message, to=[to_email])
+              email.send()
+              return render(request, 'account_activation/activation_sent.html', context)
             except IntegrityError:
                 form.add_error('username', 'Username is taken')
-
         context['form'] = form
     return render(request, 'accounts/signup.html', context)
 
+def activate_account(request, uidb64, token):
+    context = {}
+    try:
+        uid = force_bytes(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return render(request, 'account_activation/activated_successfully.html', context)
+    else:
+        return render(request, 'account_activation/activation_invalid.html', context)
 
 def do_login(request):
     context = {}

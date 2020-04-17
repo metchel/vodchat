f764b5867a0cdd772880c3a5fbfc6b17548c114c
Chen Yue <33853842+cyue49@users.noreply.github.com>
2020-04-17 17:45:59 -0400

    added some tests (#5)

--- vodchatserver/accounts/tests.py
+++ vodchatserver/accounts/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from accounts.forms import SignupForm
 
 # Create your tests here.
+class LoginAndRedirectTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create_user(username='test', password='aAE$Ol0w0#K(ws')
+        user.save()
+
+    def test_upload_redirect_if_not_logged_in(self):
+        response = self.client.get('/videos/upload')
+        self.assertRedirects(response, '/accounts/login?next=/videos/upload')
+
+    def test_upload_when_logged_in(self):
+        login = self.client.login(username='test', password='aAE$Ol0w0#K(ws')
+        response = self.client.get('/videos/upload')
+        self.assertEqual(str(response.context['user']), 'test')
+        self.assertEqual(response.status_code, 200)
+
+class SignUpFormErrorMessagesTest(TestCase):
+     def test_username_error_messages(self):
+        form = SignupForm()
+        self.assertEqual(form.fields['username'].error_messages, {'required':'Please enter an username.'})
+
+     def test_email_error_messages(self):
+       form = SignupForm()
+       self.assertEqual(form.fields['email'].error_messages, {'required':'Please enter your email.'})
+
+     def test_password_error_messages(self):
+       form = SignupForm()
+       self.assertEqual(form.fields['password'].error_messages, {'required':'Please enter a password.'})
+
+     def test_password_confirm_error_messages(self):
+        form = SignupForm()
+        self.assertEqual(form.fields['password_confirm'].error_messages, {'required':'Please confirm you password.'})
--- vodchatserver/videos/tests.py
+++ vodchatserver/videos/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from videos.models import Video
 
 # Create your tests here.
+class VideoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Video.objects.create(title='A video', description='This is a video')
+
+    def test_title_max_length(self):
+        video = Video.objects.get(title='A video')
+        max_length = video._meta.get_field('title').max_length
+        self.assertEquals(max_length, 256)
+
+    def test_description_max_length(self):
+        video = Video.objects.get(title='A video')
+        max_length = video._meta.get_field('description').max_length
+        self.assertEquals(max_length, 4096)
+
+    def test_date_created_editable_false(self):
+        video = Video.objects.get(title='A video')
+        is_editable = video._meta.get_field('date_created').editable
+        self.assertFalse(is_editable)
+
+    def test_videofile_upload_to(self):
+        video = Video.objects.get(title='A video')
+        upload = video._meta.get_field('videofile').upload_to
+        self.assertEquals(upload, 'videos/')
+
+    def test_thumbnailfile_upload_to(self):
+        video = Video.objects.get(title='A video')
+        upload = video._meta.get_field('thumbnailfile').upload_to
+        self.assertEquals(upload, 'thumbnails/')
